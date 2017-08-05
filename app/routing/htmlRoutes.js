3f2ce9e1ff42fbe1109e208909ad7c7c553b9756
kamoltip <plawansusu@gmail.com>
2017-08-05 10:00:03 -0400

    bug fixed

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -9,5 +9,9 @@ module.exports = function(app){
 		res.sendFile(path.join(__dirname, "/../public/home.html"));
 	});
 
+	app.get("*",function(req, res){
+		res.sendFile(path.join(__dirname, "/../public/home.html"));
+	});
+
 	
 }
