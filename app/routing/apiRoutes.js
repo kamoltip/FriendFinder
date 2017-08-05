ff7c837bf9cbad954fb56186820cce9f3acf8ed9
kamoltip <plawansusu@gmail.com>
2017-08-05 04:12:31 -0400

    add php file

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,7 +5,7 @@ var scoreDifference = 0;
 
 module.exports = function(app){
 		app.get('./api/friends', function(req ,res){
-				res.json(friends);
+				res.json(friendsData);
 		});
 
 		app.post('./api/friends', function(req, res){
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var path = require('path');
 var bodyParser = require('body-parser');
 
 var app = express();
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
