db65cad5d5f9798103c5e8a9cba967ccdf308f3c
kamoltip <plawansusu@gmail.com>
2017-08-07 00:54:05 -0400

    api page deploy

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,46 +1,48 @@
-var friendData = require('../data/friends.js');
+var friendData = require('../data/friends');
 var path = require('path');
 
 var scoreDifference = 0;
 
 module.exports = function(app){
-		app.get('./api/friends', function(req ,res){
+		app.get('/api/friends', function(req ,res){
 				res.json(friendData);
 		});
 
-		app.post('./api/friends', function(req, res){
+			
 
-			var matching = {
-				name: '',
-				image: '',
-				matchDifference: 1000
-			};
-			var usrData = req.body;
-			var usrName = usrData.name;
-			var usrImage = usrData.Image;
-			var usrScore = usrData.scores;
+			
 
-			var scoreDifference = 0;
+		// 	var matching = {
+		// 		name: '',
+		// 		image: '',
+		// 		matchDifference: 1000
+		// 	};
+		// 	var usrData = req.body;
+		// 	var usrName = usrData.name;
+		// 	var usrImage = usrData.Image;
+		// 	var usrScore = usrData.scores;
 
-			for (var i = 0 ; i < [friends].length-1 ; i++) {
-				console.log(friends[i].name);
-				scoreDifference = 0;
+		// 	var scoreDifference = 0;
 
-				for (var z = 0 ; z < 10; z++) {
-					scoreDifference += Math.abs(parseInt(usrScores[z]) - parseInt(friends[i].scores[z]));
+		// 	for (var i = 0 ; i < [friends].length-1 ; i++) {
+		// 		console.log(friends[i].name);
+		// 		scoreDifference = 0;
+
+		// 		for (var z = 0 ; z < 10; z++) {
+		// 			scoreDifference += Math.abs(parseInt(usrScores[z]) - parseInt(friends[i].scores[z]));
 				
-				if (scoreDifference <= matching.friendDifference){
+		// 		if (scoreDifference <= matching.friendDifference){
 
-					// Reset the bestMatch to be the new friend. 
-					matching.name = friends[i].name;
-					matching.photo = friends[i].photo;
-					matching.matchDifference = scoreDifference;
-				}
-			}
-		}
+		// 			// Reset the bestMatch to be the new friend. 
+		// 			matching.name = friends[i].name;
+		// 			matching.photo = friends[i].photo;
+		// 			matching.matchDifference = scoreDifference;
+		// 		}
+		// 	}
+		// }
 
-		friends.push(usrData);
+		// friends.push(usrData);
  
-		res.json(matching);
-	});
+		// res.json(matching);
+	// });
 };
